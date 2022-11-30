1e59833034b0296dc5c14fc95504c113826df31d
brunojacob <brunojacob_89@hotmail.com>
2022-11-30 12:37:29 -0300

    CRUD PHP

---
+++ CrudPHP/config.php
@@ -0,0 +1,11 @@
+<?php
+define('HOST', 'localhost:3307');
+define('USER', 'root');
+define('PASS', '');
+define('BASE', 'cadastro');
+
+$conn = new MYSQLi(HOST,USER,PASS,BASE);
+
+
+
+?>
---
+++ CrudPHP/salvar-usuario.php
@@ -0,0 +1,62 @@
+<?php
+    switch($_REQUEST["acao"]){
+        case 'cadastrar':
+            $nome = $_POST["nome"];
+            $email = $_POST["email"];
+            $senha = md5($_POST["senha"]);
+            $data_nascimento = $_POST["data_nascimento"];
+
+            $sql = "INSERT INTO usuarios (nome, email, senha, data_nascimento) VALUES (
+                '{$nome}', '{$email}', '{$senha}', '{$data_nascimento}')";
+            
+            $res = $conn -> query($sql);
+
+            if($res == true){
+                print "<script> alert ('Cadastro com sucesso');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }else {
+                print "<script> alert ('Não Foi Possivel cadastrar');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }
+        break;
+
+        case 'editar':
+            $nome = $_POST["nome"];
+            $email = $_POST["email"];
+            $senha = md5($_POST["senha"]);
+            $data_nascimento = $_POST["data_nascimento"];
+
+            $sql = "UPDATE usuarios SET nome = '{$nome}', email = '{$email}', senha = '{$senha}', data_nascimento = '{$data_nascimento}' WHERE id =".$_REQUEST["id"]; 
+
+            $res = $conn -> query($sql);
+
+            if($res == true){
+                print "<script> alert ('Editado com sucesso');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }else {
+                print "<script> alert ('Não Foi Possivel editar');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }
+
+            break;
+
+        case 'excluir':
+
+            $sql = "DELETE FROM usuarios WHERE id=".$_REQUEST["id"];
+
+            $res = $conn -> query($sql);
+
+            if($res == true){
+                print "<script> alert ('Exluido com sucesso');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }else {
+                print "<script> alert ('Não Foi Possivel excluir');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }
+
+
+            break;
+    }
+
+
+?>
